ef931faac25a49c1c2cf4a1e528d6dfb619d7275
Steph MANESCHI <stephanie.maneschi@hotmail.fr>
2022-09-06 23:08:27 +0200

    Add controller, service, dao and repository for Patient

---
+++ src/main/java/com/example/cabinetmedicalback/DAO/PatientDAO.java
@@ -0,0 +1,28 @@
+package com.example.cabinetmedicalback.DAO;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document("Patient")
+public class PatientDAO {
+
+    @Id
+    private String id;
+
+    private String nomPatient;
+
+    private String prenomPatient;
+
+    private String dateNaissance;
+
+    private String sexe;
+
+    private String adresse;
+
+    private String numeroSecu;
+
+    private boolean active;
+
+}
---
+++ src/main/java/com/example/cabinetmedicalback/service/PatientService.java
@@ -0,0 +1,87 @@
+package com.example.cabinetmedicalback.service;
+
+import com.example.cabinetmedicalback.DAO.PatientDAO;
+import com.example.cabinetmedicalback.repository.PatientRepository;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PatientService {
+
+    private PatientRepository repo;
+
+    @Autowired
+    public PatientService(PatientRepository repo) {
+        this.repo = repo;
+    }
+
+    /**
+     * Return the list of all the patients
+     * @return
+     */
+    public List<PatientDAO> getAllPatient() {
+        if (this.repo.findAll().size() == 0) {
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+        } else {
+            return (this.repo.findAll());
+        }
+    }
+
+    /**
+     * Return a patient from the id
+     * @param id
+     * @return The patient found
+     */
+    public PatientDAO getPatientById(String id) {
+        return (this.repo.findById(id).get());
+    }
+
+    /**
+     * Create a new patient
+     * @param newPatient the new patient which has to be added in the DB
+     * @return
+     */
+    public PatientDAO createPatient(PatientDAO newPatient) {
+        return (this.repo.insert(newPatient));
+    }
+
+    /**
+     * Update a patient
+     * Only adresse, sexe and dateNaissance can be updated
+     * @param id the id of item which has to be updated
+     * @param item the new value of the item
+     * @return
+     */
+    public PatientDAO updatePatient(String id, PatientDAO item) {
+        PatientDAO itemToUpdate = this.repo.findById(id).get();
+
+        itemToUpdate.setNomPatient(item.getNomPatient());
+        itemToUpdate.setPrenomPatient(item.getPrenomPatient());
+        itemToUpdate.setDateNaissance(item.getDateNaissance());
+        itemToUpdate.setSexe(item.getSexe());
+        itemToUpdate.setAdresse(item.getAdresse());
+        itemToUpdate.setNumeroSecu(item.getNumeroSecu());
+
+        return(this.repo.save(itemToUpdate));
+    }
+
+    /**
+     * Delete a patient
+     * Consist in changing the patient from active to inactive
+     * @param id
+     */
+    public void deletePatient(String id) {
+        PatientDAO itemToDelete = this.repo.findById(id).get();
+        itemToDelete.setActive(false);
+        this.repo.save(itemToDelete);
+    }
+
+
+
+}
