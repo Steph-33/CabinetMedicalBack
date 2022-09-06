d82780731251066d043b15103093e3a27cf64472
Stéphane PATERNA <paterna@club-internet.fr>
2022-09-06 15:11:16 +0200

    First Commit

---
+++ src/test/java/com/example/cabinetmedicalback/CabinetMedicalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cabinetmedicalback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinetMedicalBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
