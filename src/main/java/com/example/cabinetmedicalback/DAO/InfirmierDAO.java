6a7214d3b6368830dadece2e9ab3211aa538cb1d
Axel CALTE <calteaxelc@gmail.com>
2022-09-09 12:15:28 +0200

    Add activateInfirmier controller and service

--- src/main/java/com/example/cabinetmedicalback/DAO/InfirmierDAO.java
+++ src/main/java/com/example/cabinetmedicalback/DAO/InfirmierDAO.java
@@ -2,6 +2,7 @@ package com.example.cabinetmedicalback.DAO;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
